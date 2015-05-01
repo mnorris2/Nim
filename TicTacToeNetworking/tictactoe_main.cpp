6d7aed7b70ec8c729f54a25207e4427c74f17686
mnorris2 <mnorris2@harding.edu>
2015-05-01 17:20:27 -0500

    Nim

---
+++ TicTacToeNetworking/tictactoe_main.cpp
@@ -0,0 +1,11 @@
+/************************************************************************************
+* tictacetoe_main.cpp                                                               *
+*                                                                                   *
+* This program plays tictactoe over the network (both as client & server). *
+*************************************************************************************/
+#include "TicTacToe.h"
+#include <WinSock2.h>
+#include <iostream>
+
+#pragma comment (lib, "ws2_32.lib")
+
